61f49eaebcd2e558b3b17aad1a1e776d03be4b92
Bogomaz Dmitry <bogomaz.dima2013@gmail.com>
2020-09-05 16:16:20 +0300

    В разработке

---
+++ 4/4/4.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+
+class Deckan;
+
+class Group
+{
+private:
+    static int numberOfLastCreatedGroup;
+    int numberOfGroup;
+    Group(int numberOfCreatedGroup);
+public:
+    friend class Deckan;
+    int get_numberOfGroup() const;
+};
+
+int Group::numberOfLastCreatedGroup = 0;
+
+class Deckan
+{
+public:
+    Group create_group(int numberOfGroup);
+    void set_numberOfGroup(Group& gr, int newNumberOfGroup);
+    int get_numberOfLastCreatedGroup() const;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    Deckan D;
+    Group gr = D.create_group(914301);
+    Group gr1 = D.create_group(914302);
+    std::cout << "Ноер группы: " << gr.get_numberOfGroup() << std::endl;
+    std::cout << "Ноер последней созданной группы: " << D.get_numberOfLastCreatedGroup() << std::endl;
+    system("pause");
+    return 0;
+}
+
+Group::Group(int numberOfCreatedGroup):
+    numberOfGroup(numberOfCreatedGroup)
+{
+    numberOfLastCreatedGroup = numberOfCreatedGroup;
+}
+
+int Group::get_numberOfGroup() const
+{
+    return numberOfGroup;
+}
+
+Group Deckan::create_group(int numberOfGroup)
+{
+    return Group(numberOfGroup);
+}
+
+void Deckan::set_numberOfGroup(Group& gr, int newNumberOfGroup)
+{
+
+    if (Group::numberOfLastCreatedGroup == gr.numberOfGroup)
+    {
+    Group::numberOfLastCreatedGroup = newNumberOfGroup;
+    }
+    gr.numberOfGroup = newNumberOfGroup;
+}
+
+int Deckan::get_numberOfLastCreatedGroup() const
+{
+    return Group::numberOfLastCreatedGroup;
+}
---
+++ 5/5/5.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+class Transport
+{
+private:
+    double priceOfPassanger;
+    double priceOfPayload;
+    int speed;
+
+public:
+
+};
+
+class Autobus : public Transport
+{
+
+};
+
+class Bicycle : public Transport
+{
+
+};
+
+class Cart : public Transport
+{
+
+};
+
+int main()
+{
+    std::cout.unsetf(std::ios::dec);
+    std::cout.setf(std::ios::binary);
+    std::cout << 5151151;
+}
+
